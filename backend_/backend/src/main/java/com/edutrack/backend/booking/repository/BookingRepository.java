2f66b76f7fe29f3f34f2be2abf8832eb82d4726a
Brammawaxanan <sbwaxan@gmail.com>
2026-04-07 12:14:43 +0530

    add booking exception

--- backend_/backend/src/main/java/com/edutrack/backend/booking/repository/BookingRepository.java
+++ backend_/backend/src/main/java/com/edutrack/backend/booking/repository/BookingRepository.java
@@ -5,7 +5,6 @@ import com.edutrack.backend.booking.entity.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
